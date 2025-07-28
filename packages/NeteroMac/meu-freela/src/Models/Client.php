28efe8dece7ae56cedd3973af075d0819bad9bf4
NeteroMac <marcooantonioo.cardoso@gmail.com>
2025-07-28 14:52:31 -0400

    Feat: Ajustes

--- packages/NeteroMac/meu-freela/src/Models/Client.php
+++ packages/NeteroMac/meu-freela/src/Models/Client.php
@@ -2,11 +2,11 @@
 
 namespace NeteroMac\MeuFreela\Models;
 
+use NeteroMac\MeuFreela\Database\Factories\ClientFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use NeteroMac\MeuFreela\Database\Factories\ClientFactory;
 use NeteroMac\MeuFreela\Models\Project; 
 
 class Client extends Model
