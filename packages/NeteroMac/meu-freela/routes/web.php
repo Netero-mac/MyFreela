1fed3c19da92ce48e92495efe98fb9583a457e8b
Netero-mac <marcooantonioo.cardoso@gmail.com>
2025-08-14 23:35:20 -0400

    feat: Ajuate no erro 403 na Fatura

--- packages/NeteroMac/meu-freela/routes/web.php
+++ packages/NeteroMac/meu-freela/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+//packages/NeteroMac/meu-freela/routes/web.php
+
 use Illuminate\Support\Facades\Route;
 use NeteroMac\MeuFreela\Http\Controllers\ClientController;
 use NeteroMac\MeuFreela\Http\Controllers\ProjectController;
@@ -8,6 +10,13 @@ use NeteroMac\MeuFreela\Http\Controllers\InvoiceController;
 Route::middleware(['web', 'auth'])->group(function () {
     Route::resource('clients', ClientController::class);
     Route::resource('projects', ProjectController::class);
-    Route::resource('invoices', InvoiceController::class)->only(['index', 'store']);
+
+    // Rota para a listagem de faturas (continua igual)
+    Route::get('invoices', [InvoiceController::class, 'index'])->name('invoices.index');
+
+    // Rota CORRETA para criar uma fatura, associada a um projeto
+    Route::post('projects/{project}/invoices', [InvoiceController::class, 'store'])->name('invoices.store');
+    
+    // Rota para download (continua igual)
     Route::get('invoices/{invoice}/download', [InvoiceController::class, 'download'])->name('invoices.download');
 });
